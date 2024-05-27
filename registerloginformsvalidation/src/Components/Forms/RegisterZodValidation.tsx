4a7f8f53178f46a3aacc40be9f34c374beabd1c2
AaronAPerez <aaperez06@gmail.com>
2024-05-27 13:37:30 -0700

    Zod Validation

--- registerloginformsvalidation/src/Components/Forms/RegisterZodValidation.tsx
+++ registerloginformsvalidation/src/Components/Forms/RegisterZodValidation.tsx
@@ -42,25 +42,25 @@ type FormData = z.infer<typeof schema>;
 // }
 
 
-// Minimum 8 characters, at least one uppercase letter, one lowercase letter, one number and one special character
+// // Minimum 8 characters, at least one uppercase letter, one lowercase letter, one number and one special character
 // const passwordValidation = new RegExp(
-  //   /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
-  // );
+//     /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+//   );
   
-  // const validationSchema = z.object({
-  //   email: z
-  //     .string()
-  //     .min(1, { message: 'Must have at least 1 character' })
-  //     .email({
-  //       message: 'Must be a valid email',
-  //     }),
-  //   password: z
-  //     .string()
-  //     .min(1, { message: 'Must have at least 1 character' })
-  //     .regex(passwordValidation, {
-  //       message: 'Your password is not valid',
-  //     }),
-  // });
+//   const validationSchema = z.object({
+//     email: z
+//       .string()
+//       .min(1, { message: 'Must have at least 1 character' })
+//       .email({
+//         message: 'Must be a valid email',
+//       }),
+//     password: z
+//       .string()
+//       .min(1, { message: 'Must have at least 1 character' })
+//       .regex(passwordValidation, {
+//         message: 'Your password is not valid',
+//       }),
+//   });
 
 
 
